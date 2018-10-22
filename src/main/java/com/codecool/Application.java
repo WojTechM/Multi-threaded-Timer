f9f9b8e86245aafdd846daaaf8e200d041ec5ece
WojTechM <wojciech.m1997@gmail.com>
2018-10-22 17:18:45 +0200

    Add functionality to Application class - creating new timers

--- src/main/java/com/codecool/Application.java
+++ src/main/java/com/codecool/Application.java
@@ -1,15 +1,44 @@
 package com.codecool;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Application {
 
     private boolean isAppRunning = true;
+    private Map<String, Timer> timers = new HashMap<>();
 
     void run() {
         while (isAppRunning) {
             UserInput input = InputManager.getString();
             if (input.isExitRequest()) {
                 exit();
+            } else if (input.isStartRequest()) {
+                handleStartRequest(input);
             }
+        }
+    }
+
+    private void handleStartRequest(UserInput input) {
+        String timerName = input.getTimerName();
+        if (timers.containsKey(timerName)) {
+            unpauseTimer(timerName);
+        } else {
+            createTimer(timerName);
+        }
+    }
+
+    private void createTimer(String timerName) {
+        Timer timer = new Timer();
+        Thread thread = new Thread(timer);
+        timers.put(timerName, timer);
+        thread.start();
+    }
+
+    private void unpauseTimer(String timerName) {
+        Timer timer = timers.get(timerName);
+        if (!timer.isRunning()) {
+            timer.toggleTimer();
         }
     }
 
