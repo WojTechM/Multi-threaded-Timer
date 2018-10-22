26edf6edca6a0351f63ea6bec220a9975db997ec
WojTechM <wojciech.m1997@gmail.com>
2018-10-22 17:50:49 +0200

    Create class TimersDisplay

---
+++ src/main/java/com/codecool/TimersDisplay.java
@@ -0,0 +1,14 @@
+package com.codecool;
+
+import java.util.Map;
+
+public class TimersDisplay {
+
+    public void displayTimers(Map<String, Timer> timerMap) {
+        for(String key : timerMap.keySet()) {
+            Timer timer = timerMap.get(key);
+            String template = "Name: %s, ThreadId: %s, Seconds: %s";
+            System.out.println(String.format(template, key, timer.getId(), timer.getSecondsPassed()));
+        }
+    }
+}
